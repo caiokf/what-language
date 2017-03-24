c3a0cb93cbbdd20bc640d9450a5ebce7a68b2333
Caio Kinzel Filho <caiokf@gmail.com>
2017-03-24 19:04:51 +0200

    immutable js redux logger

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import Router from "./router";
+import Router from './router';
 
-ReactDOM.render(Router, document.getElementById("root"));
+ReactDOM.render(Router, document.getElementById('root'));
--- client/store.js
+++ client/store.js
@@ -1,10 +1,26 @@
+import Immutable from 'immutable';
 import { applyMiddleware, createStore } from 'redux';
-
-import logger from 'redux-logger';
+import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import reducers from './reducers';
 
-const middleware = applyMiddleware(thunk, logger());
+const logger = createLogger({
+  stateTransformer: (state) => {
+    let newState = {};
+
+    for (var i of Object.keys(state)) {
+      if (Immutable.Iterable.isIterable(state[i])) {
+        newState[i] = state[i].toJS();
+      } else {
+        newState[i] = state[i];
+      }
+    };
+
+    return newState;
+  }
+});
+
+const middleware = applyMiddleware(thunk, logger);
 
 export default createStore(reducers, middleware);
