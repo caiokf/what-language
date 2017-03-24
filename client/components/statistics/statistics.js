b56ef44efe99c6069a47081ae414aaf7d41b20ee
Caio Kinzel Filho <caiokf@gmail.com>
2017-03-25 01:10:37 +0200

    formatting numbers

--- client/components/statistics/statistics.js
+++ client/components/statistics/statistics.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React from 'react';
+import NumberFormat from 'react-number-format';
 import { connect } from 'react-redux';
 import './statistics.sass';
 
 class Statistics extends React.Component {
   render() {
+    const peoplePercentage = (this.props.howManyPeople/this.props.totalPeople) * 100;
+    const countriesPercentage = (this.props.howManyCountries/this.props.totalCountries) * 100
     return (
       <div className="statistics">
         <h3>
-          You can speak to: {this.props.howManyPeople} people ({ (this.props.howManyPeople/this.props.totalPeople) * 100 }%)<br />
-          in {this.props.howManyCountries} countries ({ (this.props.howManyCountries/this.props.totalCountries) * 100 }%)
+          <span>You can speak to: </span>
+          <NumberFormat
+            value={this.props.howManyPeople}
+            displayType={'text'}
+            thousandSeparator={true} />
+          <span> people (</span>
+          <NumberFormat
+            value={peoplePercentage}
+            displayType={'text'}
+            decimalPrecision={2}
+            suffix={'%'} />
+          <span>)</span>
+          <br />
+
+          <span>in </span>
+          <NumberFormat
+            value={this.props.howManyCountries}
+            displayType={'text'}
+            thousandSeparator={true} />
+          <span> countries (</span>
+          <NumberFormat
+            value={countriesPercentage}
+            displayType={'text'}
+            decimalPrecision={2}
+            suffix={'%'} />
+          <span>)</span>
         </h3>
       </div>
     );
