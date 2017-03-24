21af431a39222caabaa9b2dfac08d4cb54f06c19
Caio Kinzel Filho <caiokf@gmail.com>
2017-03-24 19:50:39 +0200

    language input focus

--- client/components/settings/settings.language.input.js
+++ client/components/settings/settings.language.input.js
@@ -4,11 +4,25 @@ import { connect } from 'react-redux';
 import './settings.language.input.sass';
 
 class SettingsLanguageInput extends React.Component {
+  componentWillReceiveProps() {
+     setTimeout(
+       (() => { this.languageInput.focus(); }).bind(this), 500
+     );
+  }
+
   render() {
     return (
       <form className="settings__form" action="">
         <span className="settings__info settings__label">What languages can you speak?</span>
-        <input className="settings__input" name="settings" type="text" placeholder="" autoComplete="off" autoCorrect="off" autoCapitalize="off" spellCheck="false" />
+        <input className="settings__input"
+          name="language"
+          type="text"
+          placeholder=""
+          autoComplete="off"
+          autoCorrect="off"
+          autoCapitalize="off"
+          spellCheck="false"
+          ref={(input) => { this.languageInput = input; }} />
         <span className="settings__info">Hit enter to settings or ESC to close</span>
       </form>
     );
@@ -17,6 +31,7 @@ class SettingsLanguageInput extends React.Component {
 
 const mapStateToProps = (state) => {
   return {
+    opened: state.settings.get('opened'),
     languagesSpoken: state.user.get('languagesSpoken'),
   };
 };
